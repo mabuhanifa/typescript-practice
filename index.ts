9fe5c975683bac52e62cea4323b8adab4f14754f
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-12 20:11:33 +0600

    object

--- index.ts
+++ index.ts
@@ -3,3 +3,13 @@
 let abc: string = "document";
 let num: number = 45;
 let statement: boolean = true;
+
+let user: {
+  name: string;
+  age: number;
+  email: string;
+} = {
+  name: "Mohammed Abu Hanifa",
+  age: 26,
+  email: "moh.abuhanifa@gmail.com",
+};
