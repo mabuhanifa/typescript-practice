e9317725cf51de78f711b0198c929941334f6dd5
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-21 21:39:26 +0600

    extends {}

--- generics.ts
+++ generics.ts
@@ -15,3 +15,14 @@ let user = addId({
 //  let abc = addId(1); Argument of type 'number' is not assignable to parameter of type 'object'
 // let abc2 = addId("1"); Argument of type 'string' is not assignable to parameter of type 'object'
 // let abc4 = addId({ a: [1] }); ok
+
+const addId2 = <T extends { name: string; age: number }>(obj: T) => {
+  let id = Math.floor(Math.random() * 100);
+  return { ...obj, id };
+};
+
+let user5 = addId({
+  name: "Mashrafi" /**   @name should always be there **/,
+  age: 36 /**   @age should always be there **/,
+  country: "Bangladesh", // adding extra variable will be added later by generics
+});
