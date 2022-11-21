8c17f9c0fcbd25ebc7f6d2e8e8556365be47da37
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-21 21:25:17 +0600

    T

--- generics.ts
+++ generics.ts
@@ -1,1 +1,13 @@
 //generics
+const addId = <T>(obj: T) => {
+  let id = Math.floor(Math.random() * 100);
+  return { ...obj, id };
+};
+
+let user = addId({
+  name: "Mashrafi",
+  age: 36,
+  country: "Bangladesh",
+});
+
+// user. -> all properties will be available
