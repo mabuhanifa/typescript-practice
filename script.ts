3b1998b3785f9cb3c3eefcc675c3238bd9e1bd55
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-23 16:47:15 +0600

    array

--- script.ts
+++ script.ts
@@ -21,10 +21,24 @@ let fruits = ["apple", "banana", "orange"]; // * string[] *//
 fruits.push("lichi"); //okay
 // fruits.push(9); // -> error
 
-let mixed = ["apple", "banana", 9];
+let mixed = ["apple", "banana", 9]; // * (string | number)[] *//
 
 mixed.push("apple"); //okay
 mixed.push(15); //okay
 // mixed.push(true); //error
+
+let mixed2 = ["apple", "banana", 9, false]; // * (string | number | boolean)[] *//
+
+mixed2.push("apple"); //okay
+mixed2.push(15); //okay
+mixed2.push(true); //okay
+// mixed2.push({});  //error
+
+let mixed3 = ["apple", "banana", 9, false, {}]; // * ({}[])[] *//
+
+mixed3.push("apple"); //okay
+mixed3.push(15); //okay
+mixed3.push(true); //okay
+mixed3.push({}); //okay
 
 export {};
