f3be81f6b0670cf5eb148cc5edce97a0a64a030e
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-23 16:37:34 +0600

    function

--- script.ts
+++ script.ts
@@ -1,9 +1,17 @@
-let playerName;
+let playerName; //Variable 'playerName' implicitly has an 'any' type, but a better type may be inferred from usage
 
 console.log(playerName); //undefined
 
 playerName = "playerName";
 
 playerName = 15; // no error
+
+//**   -----------------------------------------------  */
+
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+console.log(multiply(1, 5));
 
 export {};
