b42c01f870c01f9e36d55cfa88ac68ad932d0a1e
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-23 23:58:05 +0600

    addFunction

--- functionSignature.ts
+++ functionSignature.ts
@@ -1,1 +1,9 @@
+let addFunction: (x: number, y: number) => number;
 
+addFunction = (
+  x: number,
+  y: number
+  /* z: string can not take a new parameter here without declaring it in signature */
+) => {
+  return x * y;
+};
