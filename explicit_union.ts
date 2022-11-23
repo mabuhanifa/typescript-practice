3387f34bd4d03b2d0510342fe13e78f7aa25d9cd
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-23 17:07:25 +0600

    Object

--- explicit_union.ts
+++ explicit_union.ts
@@ -13,3 +13,15 @@ let ab: (string | number)[] = [];
 ab.push("abc"); // ok
 ab.push(9); // ok
 // ab.push(true); // error -> Argument of type 'boolean' is not assignable to parameter of type 'string | number'
+
+//*** @apply   ----------------------Object-------------------------  */
+
+let c: object;
+
+c = { ab: "abc", bc: 9, cd: true }; //okay
+
+let d: {
+  name: string;
+  age: number;
+  adult: boolean;
+};
