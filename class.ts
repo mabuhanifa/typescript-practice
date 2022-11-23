893fb9dcce5213e16801cb9da0851d662bd2d3dc
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-24 02:51:34 +0600

    class as type

---
+++ class.ts
@@ -0,0 +1,25 @@
+class Player {
+  name: string;
+  age: number;
+  country: string;
+
+  constructor(name: string, age: number, country: string) {
+    this.name = name;
+    this.age = age;
+    this.country = country;
+  }
+  play() {
+    console.log(`${this.name} from ${this.country} is playing`);
+  }
+}
+
+const mashrafi = new Player("Mashrafi", 40, "Bangladesh");
+const sakib = new Player("Sakib", 40, "Bangladesh");
+
+// Class can be used as a type in typescript
+
+const players: Player[] = []; // Class Player is used as a type
+
+players.push(sakib); //okay
+players.push(mashrafi); //okay
+// players.push("sakib"); //error
