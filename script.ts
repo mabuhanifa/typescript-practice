768232d64a92cb458d54418cae0b13b23497d4fd
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-23 16:42:22 +0600

    mixed array

--- script.ts
+++ script.ts
@@ -6,12 +6,25 @@ playerName = "playerName";
 
 playerName = 15; // no error
 
-//**   -----------------------------------------------  */
+//**   ----------------------function-------------------------  */
 
 function multiply(a: number, b: number) {
   return a * b;
 }
 
 console.log(multiply(1, 5));
+
+//**   ----------------------Array-------------------------  */
+
+let fruits = ["apple", "banana", "orange"]; // * string[] *//
+
+fruits.push("lichi"); //okay
+// fruits.push(9); // -> error
+
+let mixed = ["apple", "banana", 9];
+
+mixed.push("apple"); //okay
+mixed.push(15); //okay
+// mixed.push(true); //error
 
 export {};
