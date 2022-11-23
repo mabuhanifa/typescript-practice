52e8a772c4640a2fe6b7074de5e2089879ee3b0b
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-23 23:32:29 +0600

    type updated

--- functionSignature.ts
+++ functionSignature.ts
@@ -1,10 +1,1 @@
-type stringOrNumber = string | number;
-type userType = { name: string; age: number };
 
-const userData = (id: stringOrNumber, user: userType) => {
-  console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`);
-};
-
-const sayHello = (user: userType) => {
-  console.log(`User name is ${user.name} and age is ${user.age}`);
-};
