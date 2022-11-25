671b92508e0969f8091c0ad0383b291ed6c50605
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-25 22:25:23 +0600

    RectangleOptions

--- interface.ts
+++ interface.ts
@@ -1,1 +1,15 @@
+interface RectangleOptions {
+  width: number;
+  height: number;
+}
+
+function drawRectangle(options: RectangleOptions) {
+  let width = options.width;
+  let height = options.height;
+}
+
+drawRectangle({
+  height: 5,
+  width: 5,
+});
 export {};
