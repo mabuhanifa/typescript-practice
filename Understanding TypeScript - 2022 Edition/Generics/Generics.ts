4c56403a420b54b57c5185ef64b1434ca8e82340
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-10 03:32:17 +0600

    promise

--- Understanding TypeScript - 2022 Edition/Generics/Generics.ts
+++ Understanding TypeScript - 2022 Edition/Generics/Generics.ts
@@ -1,2 +1,8 @@
 const names: Array<string> = []; /* Array<string> === string[] */
 names[0].split("."); /* All string methods will be available  */
+
+const promise = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve("this is done");
+  }, 200);
+});
