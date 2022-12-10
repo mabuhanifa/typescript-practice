986d0f7d6b22fa8e1f68c38af2b2ff41060f2a39
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-10 07:12:40 +0600

    Lengthy

--- Understanding TypeScript - 2022 Edition/Generics/function.ts
+++ Understanding TypeScript - 2022 Edition/Generics/function.ts
@@ -4,3 +4,17 @@ function merge<T extends {}, U>(objA: T, objB: U) {
 
 const newObj = merge({ name: "a" }, { age: 30 });
 newObj.age;
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let description = "Got No Value";
+  if (element.length === 1) {
+    description = "Got 1 element";
+  } else if (element.length > 1) {
+    description = `Got  ${element.length} element`;
+  }
+  return [element, description];
+}
