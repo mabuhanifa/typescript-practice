fa267f518f4dc1d4b74bea58b76516a6be0037e2
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-12 01:33:41 +0600

    Partial

---
+++ Understanding TypeScript - 2022 Edition/Generics/utilityTypes.ts
@@ -0,0 +1,18 @@
+interface CourseGoal {
+  titles: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function completeCourse(
+  titles: string,
+  description: string,
+  completeUntil: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  // Partial will makes CourseGoal properties optional
+  (courseGoal.titles = titles),
+    (courseGoal.description = description),
+    (courseGoal.completeUntil = completeUntil);
+  return courseGoal as CourseGoal;
+}
