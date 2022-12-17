0ef48e7b6ee4e4219dc9b95baaa65934b82b93c4
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-17 23:49:13 +0600

    ReadonlyCat

--- Utility Types/ReadOnly.ts
+++ Utility Types/ReadOnly.ts
@@ -1,14 +1,22 @@
 interface Todo {
   title: string;
 }
+
 interface Cat {
   breed: string;
 }
+type ReadonlyCat = Readonly<Cat>;
 
 const todo: Readonly<Todo> = {
   title: "Delete inactive users",
 };
 
 // todo.title = "Hello"; -> Cannot assign to 'title' because it is a read-only property.
+
+const cat: ReadonlyCat = {
+  breed: "string",
+};
+
+// cat.breed = "shortHair"; -> Cannot assign to breed because it is a read-only property.
 
 export {};
